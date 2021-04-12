084ca01a4c8e7da6dedc3f65b7ceb19b6dbd5f47
tiagorahal <rahal.aires@gmail.com>
2021-04-12 16:28:00 -0300

    Dog years project add

---
+++ dog-years/dog-years.js
@@ -0,0 +1,18 @@
+let myAge = 27;
+
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeImDogYears =  earlyYears + laterYears;
+
+let myName = "Tiago".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeImDogYears} years old in dog years.`)
