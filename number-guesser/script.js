7d0f18a8c59c40c79d731a79d94e4d565d4fbe79
tiagorahal <rahal.aires@gmail.com>
2021-05-28 13:59:19 -0300

    Function to compare the player numbers and the computer number

--- number-guesser/script.js
+++ number-guesser/script.js
@@ -4,6 +4,19 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-const generateTarget() {
+function generateTarget() {
   return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(playerNumber, computerNumber, randomNumber) {
+  if(playerNumber === randomNumber && computerNumber !== randomNumber){
+    return true
+  } else if(computerNumber === randomNumber && playerNumber !== randomNumber) {
+    return false
+  } else if(playerNumber === randomNumber && computerNumber === randomNumber) {
+    return true
+  } else {
+    console.log('Something is wrong!')
+    return false
+  }
 }
